9ed16e5a71a95a9c049c575bb7b5263945310a28
widouglas <widouglasmail@gmail.com>
2022-09-21 11:24:33 -0300

    Configurando mensageria para email

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -36,14 +36,15 @@ class SeriesController extends Controller
         $serie = $this->repository->add($request);
 
         $userList = User::all();
-        foreach ($userList as $user) {
+        foreach ($userList as $index => $user) {
             $email = new SeriesCreated(
                 $serie->nome,
                 $serie->id,
                 $request->seasonsQty,
                 $request->episodesPerSeason
             );
-            Mail::to($user)->send($email);
+            $when = now()->addSeconds($index * 2);
+            Mail::to($user)->later($when, $email);
         }
 
         return to_route('series.index')
